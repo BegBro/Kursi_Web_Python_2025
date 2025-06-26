3d9ac3f33641f99f713bc4aea8c44ebc6e24afc6
BegBro <andrei.begunov322@gmail.com>
2025-06-26 15:44:29 +0300

    Factorial

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 # min, max, average,summ, production
 N = 5
-total = 0
-prod = 1
+fact = 1
 
-min_val = float('inf')  # + бесконечность
-max_val = float('-inf')  # - бесконечность
-for _ in range(N):
-    num = int(input('Введите целое число:'))
-    if num < min_val:
-        min_val = num
-    if num > max_val:
-        max_val = num
-    total += num
-    prod *= num
-    average = total / N
-
-print(f'Сумма: {total}')
-print(f'Ср. арифметическое: {average}')
-print(f'Минимум: {min_val}')
-print(f'Максимум: {max_val}')
-print(f'Произведение всех чисел:  {prod}')
+for i in range(1, N + 1):
+    fact *= i
+print(fact)
