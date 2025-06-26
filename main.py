81224ab88a77e634572f41d9519c8e62001cf239
BegBro <andrei.begunov322@gmail.com>
2025-06-26 16:18:04 +0300

    height task with additional conditions

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-# Вложенные циклы
+# Подбор по росту
+# 150 < height < 180
+# Число кандидатов
+# Число, кто прошел по критерию
+# Среди прошедших min и max
+total = 0
+total_success = 0
+min_val = float('inf')
+max_val = float('-inf')
 
-for i in range(1,10):
-    for j in range (1,10):
-        print(f'{i}*{j} = {i*j}',end='\n')
+while (height := int(input('Введите рост кандидата: '))) != -1:
+    if 150 <= height <=180:
+        total_success +=1
+        if height < min_val:
+            min_val = height
+        if height > max_val:
+            max_val = height
+    total += 1
+print(f'Минимальный рост кандидата: {min_val}')
+print(f'Максимальный рост кандидата: {max_val}')
+print(f'Кол-во прошедших кандидатов: {total_success}')
+print(f'Всего кандидатов: {total}')
