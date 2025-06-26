ca2093bcc8e9e7fdae328e64fdd645d941360619
BegBro <andrei.begunov322@gmail.com>
2025-06-26 14:52:08 +0300

    for loop begin

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-# ключевое слово in
-
+# цикл for
+# for <переменная> in iterable:
+#     команды
+"""
 word = 'поток'
 
-if 'ток' in word:
-    print('Есть.')
+for ch in word:
+    print(ch)
+"""
+#                  0    3     1
+# итератор range(start,stop,step)
+"""
+for i in range(0,3,1):
+    print(i)
+
+
+for _ in range(10):
+    print('Привет')
+"""
+
+for i in range(3,101):
+    if i % 10 ==3:
+        print(i)
