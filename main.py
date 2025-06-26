7dfc26cae4c91a441f4ecec8dd5f8f4b8a8072d4
BegBro <andrei.begunov322@gmail.com>
2025-06-26 14:12:11 +0300

    Loop height

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# break, continue
-counter = -1 # обнуляем счетчик
-# цикл из 5 операции, но 3 пропускаем
-while counter < 5:
-    counter += 1
-    if counter == 3:
-        continue
-    print(f'Итерация номер: {counter}')
+
+# while (height := int(input('Введите свой рост: '))) < 150 or height > 180:
+#     print('Вы не подходите.Позовите следующего.')
+# print('Вы подходите.Проходите.')
+
+height = int(input('Введите рост: '))
+
+while not (150 <= height <= 180):
+    print(f'Рост кандидата {height} не подходит')
+    height = int(input('Введите рост: '))
+
+print('Кандидат выбран.')
