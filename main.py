05057ccae48d27f979372fdb2e22e749d2c4b8a2
BegBro <andrei.begunov322@gmail.com>
2025-06-26 12:53:07 +0300

    Loop Continue

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 # break, continue
-num = 3 # число, которое надо угадать
-flag = True # флаг, изменяет значение по событию
-var = ''
-
-print('Я загадал число, угадай!')
-
-while True:
-    var= int(input('Ваше значение: '))
-    if var == num:
-        print('Ура. Угадал!')
-        break
-    elif var > num:
-        print('Ваше число больше загаданного.')
-    else:
-        print('Ваше число меньше загаданного.')
-print('Приходи еще!')
+counter = -1 # обнуляем счетчик
+# цикл из 5 операции, но 3 пропускаем
+while counter < 5:
+    counter += 1
+    if counter == 3:
+        continue
+    print(f'Итерация номер: {counter}')
