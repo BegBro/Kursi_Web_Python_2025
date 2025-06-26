900d4f82453a9faaf0d37668edd966a4de0a59d3
BegBro <andrei.begunov322@gmail.com>
2025-06-26 14:38:03 +0300

    Keyword "in"

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-# match - case (3.10 >)
-flag = True
-print('Возможные ходы:\n\tL - влево\n\tR - вправо\n\tF - прямо, Q - выход')
+# ключевое слово in
 
-while flag:
-    ch = input('Ваш выбор: ')
-    match ch:
-        case 'L' | 'l' | 'Д' | 'д':
-            print('Свернули налево.')
-        case 'R' | 'r' | 'К' | 'к':
-            print('Свернули направо.')
-        case 'F' | 'f' | 'А' | 'а':
-            print('Пошли прямо.')
-        case 'Q' | 'q' | 'Й' | 'й':
-            print('До свидания!')
-            flag = False
-        case _:
-            print('Выбор неясен.')
+word = 'поток'
+
+if 'ток' in word:
+    print('Есть.')
