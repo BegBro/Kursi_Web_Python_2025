1f566bab36fe31f023b0a7c2398140835b1e080f
BegBro <andrei.begunov322@gmail.com>
2025-06-27 15:59:32 +0300

    Str Methods

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-"""
-word = input('Введите фразу для зашифровки:')
-for i in range(len(word)):
-    print(ord(word[i]))
-"""
-# Зашифровываем
-s = set()
-# word = input('Введите фразу для зашифровки:')
-word = 'Пиво'
-for ch in word:
-    s.add(ord(ch))
-print(s)
 
-# Расшифровываем
-res = ''
-for i in s:
-    res += chr(i)
-print(res)
+# abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+phrase = 'Язык Python'
+
+print(phrase.lower()) # все маленькие
+print(phrase.upper()) # все большие
+print(phrase.capitalize()) # первая буква заглавная
+print(phrase.title()) # все слова с заглавной
+print('Ура! ' * 3)
+print('Телевизор'.count('е'))
+print('Python'.index('h'))
+
+"""
+['capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find',
+ 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier',
+ 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip',
+ 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition',
+ 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper',
+ 'zfill']
+"""
