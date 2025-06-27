0adc60067645c402dbef30daa421d2058fea9f50
BegBro <andrei.begunov322@gmail.com>
2025-06-27 14:27:47 +0300

    Str intro

--- main.py
+++ main.py
@@ -1,32 +1,10 @@
 """
-cards = {5, 7, 'туз','валет','дама','король'}
-print(cards)
-cards_2 = cards.copy()
-cards_2.discard('туз')
-cards_buf = cards_2 & cards
-while cards_buf:
-    print(cards_buf.pop())
-"""
-
-"""
-cards ={3, 7, 'T','D','V','K'}
-ace = {'T'}
-
-result = cards - ace
-print(result)
-"""
-
-"""
-t_is = False
-cards = {5, 7, 'туз','валет','дама','король'}
-print(cards)
-while cards:
-    card = cards.pop()
-    if card == 'туз':
-        cards.add(card)
-        t_is = True
-    else:
-        print(cards)
-    if t_is and len(cards) == 1:
-        break
+# Строки (immutable)
+#    012345 - индекс str
+s = 'Python'
+# s[3] = 'y' error (immutable)
+print(s[0])
+print(s[3])
+print(f'Длина слова: {len(s)}')
+print(s[-1]) # индекс может быть отрицательным (будет считать с конца)
 """
