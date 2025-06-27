3758b0a0785c9ce0e0b18bbc00a84f728e4e2129
BegBro <andrei.begunov322@gmail.com>
2025-06-27 15:43:23 +0300

    Encrypting

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-# Строки (immutable, iterable)
-#  Задача: исправить букву в слове сабака
+"""
+word = input('Введите фразу для зашифровки:')
+for i in range(len(word)):
+    print(ord(word[i]))
+"""
+# Зашифровываем
+s = set()
+# word = input('Введите фразу для зашифровки:')
+word = 'Пиво'
+for ch in word:
+    s.add(ord(ch))
+print(s)
 
-s = 'сабака'
+# Расшифровываем
 res = ''
-
-for i in range(len(s)):
-    if i == 1:
-        res += 'о'
-    else:
-        res += s[i]
+for i in s:
+    res += chr(i)
 print(res)
