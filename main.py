9fb647c62e7e088f78d8ab90ec0d1407fab50899
BegBro <andrei.begunov322@gmail.com>
2025-06-27 09:37:46 +0300

    Homework Lesson 2

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-# Подбор по росту
-# 150 < height < 180
-# Число кандидатов
-# Число, кто прошел по критерию
-# Среди прошедших min и max
-total = 0
-total_success = 0
-min_val = float('inf')
-max_val = float('-inf')
+# Домашняя работа
 
-while (height := int(input('Введите рост кандидата: '))) != -1:
-    if 150 <= height <=180:
-        total_success +=1
-        if height < min_val:
-            min_val = height
-        if height > max_val:
-            max_val = height
-    total += 1
-print(f'Минимальный рост кандидата: {min_val}')
-print(f'Максимальный рост кандидата: {max_val}')
-print(f'Кол-во прошедших кандидатов: {total_success}')
-print(f'Всего кандидатов: {total}')
+a = int(input('Введите вес монеты "а":'))
+b = int(input('Введите вес монеты "b":'))
+# c = input('Введите вес монеты "c"')
+
+if a == b:
+    print('Фальшивая монетка "c"')
+elif a > b:
+    print('Фальшивая монета "b"')
+else:
+    print('Фальшивая монета "a"')
