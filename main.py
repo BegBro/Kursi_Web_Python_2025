c851dae5eaebd18b13060492f3cb2bedb02af837
BegBro <andrei.begunov322@gmail.com>
2025-06-27 16:14:49 +0300

    Str Repeat Letter

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-
-# abc = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
-phrase = 'Язык Python'
-
-print(phrase.lower()) # все маленькие
-print(phrase.upper()) # все большие
-print(phrase.capitalize()) # первая буква заглавная
-print(phrase.title()) # все слова с заглавной
-print('Ура! ' * 3)
-print('Телевизор'.count('е'))
-print('Python'.index('h'))
-
 """
-['capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find',
- 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier',
- 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip',
- 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition',
- 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper',
- 'zfill']
+word = 'статор'
+res = ''
+for i in range(len(word)):
+    res += word[i]*(i+1)
+print(res)
 """
+
+word = 'статор'
+for i in range(len(word)):
+    print(word[i]*(i+1), end= '')
