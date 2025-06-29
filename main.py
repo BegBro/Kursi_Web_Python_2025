f072a1fbffcb6243e31d8971cab0509924257924
BegBro <andrei.begunov@gmail.com>
2025-06-30 00:40:37 +0300

    Homework Lesson 3

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-"""
-word = '        статор              '
-
-print(word.strip())
-print(word.lstrip())
-print(word.rstrip())
-"""
-
-temp =int(input('Введите слово: ').strip())
-word = 'ротор'
-
-print(word.strip('р')) # Убирает символ 'р' c краёв str
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+while True:
+    print('Введите А чтобы зашифровать сообщение, Б чтобы расшифровать и В чтобы выйти')
+    menu = input('>>> ').lower().strip()
+    if menu == 'в':
+        break
+    elif not (menu == 'а' or menu == 'б'):
+        continue
+    output = ''
+    message = input('Введите строку: ').lower().strip()
+    key = int(input('Введите ключ: '))
+    if menu == 'б':
+        key *= -1
+    for letter in message:
+        if letter in alphabet:
+            t = alphabet.find(letter)
+            new_key = (t + key) % len(alphabet)
+            output += alphabet[new_key]
+        else:
+            output += letter
+    print('Результат: ' + output)
