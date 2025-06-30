a11acfe9d3001503545fefe11a78d9908d2df7b7
BegBro <andrei.begunov322@gmail.com>
2025-06-30 11:13:38 +0300

    Find

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 # Строки (immutable, iterable)
 # Начало и окончание строки
-# startswith и endswith
+# 1.find('подстрока')
+# 2.find('подстрока', start)
+# 3.find('подстрока', start)
+"""
+s= 'Cмотреть, вертеть, видеть'
 
-s= 'смотреть'
+index = s.find('р') # ищем с начала строки s
+print(index)
 
-if s.startswith('смо'):
-    print('Да')
+index = s.find('р',10) # ищем с позиции __start в строке s
+print(index)
 
-if s.endswith('еть'):
-    print('Тоже да.')
+index = s.find('ер',10,15) # ищем с позиции __start до __end
+print(index)
+"""
 
+s = 'синхрофазотрон'
+ch = 'о'
+
+if ch in s:
+    count = s.count(ch)
+    print(f'Буква {ch} встречается {count} раз(а).')
+    print('Её позиция/позиции:',end=' ')
+    start = 0
+    for i in range(count):
+        pos = s.find(ch, start)
+        start = pos + 1
+        print(pos,end=' ')
+else:
+    print(f'Буквы {ch} нет в слове "{s}".')
