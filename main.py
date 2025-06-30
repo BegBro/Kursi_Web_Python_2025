49a32cc3aaff59d8c7ccd09f00f3f3851eed9716
BegBro <andrei.begunov322@gmail.com>
2025-06-30 11:23:05 +0300

    Replace

--- main.py
+++ main.py
@@ -1,32 +1,8 @@
 # Строки (immutable, iterable)
 # Начало и окончание строки
-# 1.find('подстрока')
-# 2.find('подстрока', start)
-# 3.find('подстрока', start)
-"""
-s= 'Cмотреть, вертеть, видеть'
+# 1. replace('что', 'на что') - полная замена
+# 1. replace('что', 'на что', сколько раз) - число замен
 
-index = s.find('р') # ищем с начала строки s
-print(index)
+s='+7-012-345-67-89' # => +7 (012) 345-67-89
 
-index = s.find('р',10) # ищем с позиции __start в строке s
-print(index)
-
-index = s.find('ер',10,15) # ищем с позиции __start до __end
-print(index)
-"""
-
-s = 'синхрофазотрон'
-ch = 'о'
-
-if ch in s:
-    count = s.count(ch)
-    print(f'Буква {ch} встречается {count} раз(а).')
-    print('Её позиция/позиции:',end=' ')
-    start = 0
-    for i in range(count):
-        pos = s.find(ch, start)
-        start = pos + 1
-        print(pos,end=' ')
-else:
-    print(f'Буквы {ch} нет в слове "{s}".')
+print(s.replace('-',' (',1).replace('-',') ',1))
