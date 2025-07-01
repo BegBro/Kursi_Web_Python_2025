67116c8cc842a6baa909dfaffad8da2cd7ce08a0
BegBro <andrei.begunov322@gmail.com>
2025-07-01 12:11:58 +0300

    Nested lists

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-"""
-# Списочные выражения (list comprehension)
-# squares = []
-# for i in range(10):
-#     squares.append(i ** 2)
-# Список квадратов чисел
-squares = [i**2 for i in range(10)] # на первом месте что попадёт в список, на втором закономерность
+# Вложенные списки
+# matrix =[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+# ]
 
-# список квадратов чётных чисел
-squares = [i**2 for i in range(10) if i % 2 == 0] # на первом месте что попадёт в список, на втором закономерность, на третьем условие
-#          что       закон            условие
-print(*squares, sep=', ')
+N = 3
+matrix = [[i] * N for i in range(N)]
+print(matrix)
 
-# произведение i и j
-# print([i*j for i in range(3) for j in range(3)])
+# обход 2-мерного списка (матрицы)
+count = 1
+for row in range(len(matrix)):
+    for col in range(len(matrix[row])):
+        matrix[row][col] = count
+        count += 1
 
-n = '100 200 300 400 500 600 700 800'
-approved = [500,800]
-a = [int(i) for i in n.split() if int(i) in approved]
-print(a)
-"""
-
-text = 'Списочные выражения применяются для эффективности кода'
-# text = text.split()
-# res = [a for a in text if (text.index(a)+1) % 3 == 0]
-
-res = [a for a in text.split() [2::3]]
-print(res)
+print(matrix)
