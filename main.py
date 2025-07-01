50e1cc90da441e172b10c78eb9d5e6f81969bc7a
BegBro <andrei.begunov@gmail.com>
2025-07-01 20:22:34 +0300

    Homework Lesson 5 +

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 # ДЗ: Функция, которая выводит число словами 56 -> пятьдесят шесть
 def num_to_word(num):
-    if len(str(num)) > 2:
-        return None
+    # e = num % 10
+    # g = int(num / 10)
 
-    e = num % 10
-    g = int(num / 10)
-
-    le = {1: 'один',
+    le = {0: 'ноль',
+          1: 'один',
           2: 'два',
           3: 'три',
           4: 'четыре',
@@ -16,15 +14,22 @@ def num_to_word(num):
           8: 'восемь',
           9: 'девять',
           }
+    lst = []
+    while num != 0:
+        temp = num % 10
+        num = int(num / 10)
+        if temp in le.keys():
+            lst.append(le[temp])
+    lst.reverse()
 
-    if e in le.keys():
-        e = le[e]
-    if g in le.keys():
-        g = le[g]
-    lst = [g,e]
-    return lst
+    return print(lst)
+
+    # if e in le.keys():
+    #     e = le[e]
+    # if g in le.keys():
+    #     g = le[g]
+    # lst = [g,e]
+    # return lst
 
 
-
-f = num_to_word(14)
-print(f)
+num_to_word(55889644)
