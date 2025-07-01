76f8be1c642c07f085b2a26d11a8e711efe19580
BegBro <andrei.begunov322@gmail.com>
2025-07-01 11:40:41 +0300

    Comprehension task

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
-# ДЗ
-
-# stop_list =[] # список запрещенных слов
-
-# Текст на входе, список на выходе, пронумерованный и отсортированный. Вывести разрешенные слова в список.
-
 """
-stop_list = ['я', 'море', 'чистое']
-text = 'Я люблю море Я лечу на море Я умею плавать в море Какое чистое море Хочу на море Завтра поедем на море Корабль плавает по морю'
+# Списочные выражения (list comprehension)
+# squares = []
+# for i in range(10):
+#     squares.append(i ** 2)
+# Список квадратов чисел
+squares = [i**2 for i in range(10)] # на первом месте что попадёт в список, на втором закономерность
 
-res = set(text.lower().split())
-res = list(res)
-for word in res:
-    if word in stop_list:
-        res.remove(word)
+# список квадратов чётных чисел
+squares = [i**2 for i in range(10) if i % 2 == 0] # на первом месте что попадёт в список, на втором закономерность, на третьем условие
+#          что       закон            условие
+print(*squares, sep=', ')
 
-res.sort()
+# произведение i и j
+# print([i*j for i in range(3) for j in range(3)])
 
-for i,v in enumerate(res):
-    print(f'{i+1}.{v}')
+n = '100 200 300 400 500 600 700 800'
+approved = [500,800]
+a = [int(i) for i in n.split() if int(i) in approved]
+print(a)
 """
 
-stop_list = {'я', 'море', 'чистое'}
-text = 'Я люблю море Я лечу на море Я умею плавать в море Какое чистое море Хочу на море Завтра поедем на море Корабль плавает по морю'
+text = 'Списочные выражения применяются для эффективности кода'
+# text = text.split()
+# res = [a for a in text if (text.index(a)+1) % 3 == 0]
 
-res = set(text.lower().split())
-
-res = res - stop_list
-res = sorted(res)
-
-for i,v in enumerate(res):
-    print(f'{i+1}.{v}')
+res = [a for a in text.split() [2::3]]
+print(res)
