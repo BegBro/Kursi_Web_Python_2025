2dc1ff7269cb24a0a99ffa727662c41d5de0d924
BegBro <andrei.begunov322@gmail.com>
2025-07-01 12:15:53 +0300

    Nested lists generation

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-# Вложенные списки
-# matrix =[
-#     [1,2,3],
-#     [4,5,6],
-#     [7,8,9],
-# ]
+# # Вложенные списки
+# # matrix =[
+# #     [1,2,3],
+# #     [4,5,6],
+# #     [7,8,9],
+# # ]
+#
+# N = 3
+# matrix = [[i] * N for i in range(N)]
+# print(matrix)
+#
+# # обход 2-мерного списка (матрицы)
+# count = 1
+# for row in range(len(matrix)):
+#     for col in range(len(matrix[row])):
+#         matrix[row][col] = count
+#         count += 1
+#
+# print(matrix)
 
-N = 3
-matrix = [[i] * N for i in range(N)]
-print(matrix)
+matrix = []
+table = []
 
-# обход 2-мерного списка (матрицы)
-count = 1
-for row in range(len(matrix)):
-    for col in range(len(matrix[row])):
-        matrix[row][col] = count
-        count += 1
+star = 1
+N = 8
+
+for i in range(N):
+    for j in range(star, star + N):
+        table.append(j)
+    matrix.append(table)
+    table = []
+    star += N
 
 print(matrix)
