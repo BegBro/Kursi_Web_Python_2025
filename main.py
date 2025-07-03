3ba655c73cda42c9454cfe2db5e0112898906ee1
BegBro <andrei.begunov322@gmail.com>
2025-07-03 10:26:55 +0300

    Lesson 6

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
+# Анонимные функции (однострочные, безымянные)
+# lambda-функции
+# lambda <>,
+
+
+
 # Функция, как объект
 # Передаётся в другие функции: функции высшего порядка
 
 # Функция критерия отбора элементов списка
 # Критерий: длина слова
-def is_longer_six(word):
-    return len(word) > 6
+# def is_longer_six(word):
+#     return len(word) > 6
+
+
+# is_longer_six = lambda word: len(word) > 6
 
 
 # Критерий - первая буква
-def is_first_letter_a(word):
-    return word[0] == 'а'
+# def is_first_letter_a(word):
+#     return word[0] == 'а'
+
+# is_first_letter_a = lambda word: word[0] == 'a'
 
 
-def square(num):
-    return num ** 2
+# Критерий - вхождение подстроки
+# В частности 'ан'
+# def string_contains(s):
+#     return 'ан' in s
 
 
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9] # 123456789
-squares = map(square, nums)
-print(list(squares))
-
-
-
-words = ['В', 'этом', 'списке', 'останутся', 'слова',
-         'длина', 'которых', 'больше', 'шести']
-
-fruits = ['арбуз', 'ананас', 'банан', 'ежевика', 'малина']
-
-result = list(filter(is_longer_six, words))
-print(result)
-
-res = list(filter(is_first_letter_a, fruits))
-print(res)
-
-for word in filter(is_longer_six, words):
-    print(word)
+# string_contains = lambda s: 'ан' in s
+#
+#
+# def square(num):
+#     return num ** 2
+#
+#
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9] # 123456789
+# squares = map(square, nums)
+# print(list(squares))
+#
+#
+#
+# words = ['В', 'этом', 'списке', 'останутся', 'слова',
+#          'длина', 'которых', 'больше', 'шести']
+#
+# fruits = ['арбуз', 'ананас', 'банан', 'ежевика', 'малина']
+#
+# result = list(filter(lambda word: len(word) > 6, words))
+# print(result)
+#
+# res = list(filter(lambda x: x[0] == 'а', fruits))
+# print(res)
+#
+# for word in filter(lambda word: len(word) > 6, words):
+#     print(word)
+#
+#
+# res = list(filter(lambda s: 'ан' in s, fruits))
+# print(res)
+#
 
 # def multy(first, *args):
 #     # print(len(args)) # подсчёт числа аргументов
