d93b520de95a61fab31e7bbff8dd6775dfcbff6c
BegBro <andrei.begunov322@gmail.com>
2025-07-08 12:11:12 +0300

    Exceptions by Name

--- main.py
+++ main.py
@@ -1,3 +1,54 @@
+# Исключения (runtime)
+# try:
+#   что пытаемся сделать
+# except:
+#   обрабатываем исключения
+# else:
+#   если исключения не было
+# finally:
+#   выполняется в любом случае
+
+
+
+# print('Остаток от деления:')
+# loop = True
+# while loop:
+#
+#     try:
+#         value = int(input('На что делим число 10:'))
+#         res = 10 % value
+#         print(f'Остаток от деления 10 на {value} = {res}')
+#     except ZeroDivisionError:
+#         print('На ноль делить нельзя!')
+#     except ValueError:
+#         print('Надо вводить только целые числа.')
+#     except Exception as exp: # Любое исключение в переменной exp
+#         print('Произошло исключение:', exp.__class__.__name__,'|',exp)
+#     else:
+#         loop = False
+
+###########################################
+# flag = False # открывался ли на запись
+#
+# try:
+#     fo = open('information.txt', encoding='utf-8')
+# except FileNotFoundError:
+#     fo = open('information.txt', 'wt', encoding='utf-8')
+#     flag = True
+#     print('Файл не обнаружен и создан с параметрами по умолчанию.')
+#     # with open('information.txt', 'wt', encoding='utf-8') as fo:
+#     #     fo.write('По умолчанию.')
+# else:
+#     print('Файл открыт успешно. Читаем его.')
+#     print(fo.read())
+#     fo.close()
+# finally:
+#     if flag:   # если файл был открыт на запись (было исключение)
+#         fo.write('По умолчанию.')
+#         fo.close()
+#     print('Продолжаем работать.')
+############################################
+
 # res = []
 # with open('info.txt', 'rt') as f:
 #    while temp := f.readline().rstrip('\n'):
@@ -8,8 +59,8 @@
 # res = sorted(int(x) for x in set(res))
 #
 # print(res)
-from path_lib import *
-print(img_dir)
+# from path_lib import *
+# print(img_dir)
 
 # import pickle
 # import pprint
@@ -29,4 +80,3 @@ print(img_dir)
 # with open('dictfile.dat', 'rb') as p:
 #     d = pickle.load(p)
 # pprint.pprint(d,width=15)
-
