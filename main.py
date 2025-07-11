17d80bddc164bfa40176f46210d983acfada0017
BegBro <andrei.begunov322@gmail.com>
2025-07-11 10:02:52 +0300

    Lesson_12

--- main.py
+++ main.py
@@ -1,441 +1,303 @@
-#
-class Balance:
-    def __init__(self):
-        self.right = 0
-        self.left = 0
+# OOП Class
+"""
+Задача: создать классы для моделирования клиентов банка и их счетов.
+Условия:
+	Класс BankAccount с атрибутами: _owner_name, balance.
+	Методы: deposit(amount), withdraw(amount), get_balance().
 
-    def add_left(self,weight): # в граммах
-        pass
+"""
 
-    def add_right(self,weight):
-        pass
-    def result(self) -> str:
-        return # состояние строки(левая перевесила,правая перевесила,уравновешенно)
 
-# ООП (encapsulation)
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self._owner = owner
+        self._balance = balance
 
-# Класс - тип, описывающий устройство объектов.
-# Экземпляр - объект порожденный классом.
-# Конструктор
-# Статичные члены класса
-# задачи
-from lib import Sorter
-s = Sorter()
+    def get_balance(self):
+        return self._balance
 
-s.add_word('Привет')
-s.add_word('Пока')
-s.add_word('Здорово')
-s.add_word('Здравствуй')
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
+            print(f'Депозит пополнен на сумму {amount}.')
+        else:
+            print(f'Нельзя вносить отрицательную сумму на депозит.')
 
-print(s.result())
+    def withdraw(self, amount):
+        if 0 < amount <= self._balance:
+            self._balance -= amount
+            print(f'С депозита снята сумма {amount}.')
+        else:
+            print(f'Не хватает средств. Овердрафт не доступен.')
 
-# from lib import Separator
-#
-# sep = Separator()
-#
-# for i in range(1,26):
-#     sep.add_num(i)
-#
-# print(sep.get_odd())
-# print(sep.get_even())
 
-# from lib import Clicker
-# cl = Clicker()
-#
-# cl.click()
-# cl.click()
-# cl.click()
+client1 = BankAccount('John')
+client1.deposit(500)
+client1.withdraw(600)
+print('Остаток:', client1.get_balance())
+
+# OOП (inheritance)
+# класс, от которого наследуем: базовый, родительский, суперкласс
+# класс, который наследуется: производный, дочерний
+# from math import pi
+# from abc import ABC, abstractmethod
 #
 #
-# print(cl.get_counter())
-# cl.reset()
-# print(cl.get_counter())
-
-
-# from lib import Car
+# class Shape(ABC):
+#     def info(self):
+#         print(f'Класс: {self.__class__.__name__}')
 #
-# car = Car()
+#     @abstractmethod
+#     def area(self):
+#         pass
 #
-# car.set_color('BLACK')
-# car.start_engine()
-# car.drive_to('домой')
-# print(car.get_color())
-#
-# print(car.get_model())
-
-# car1 = Car()
-# car2 = Car()
-# car3 = Car()
-# print('В парке машин: ',Car.get_counter())
-
-
-# from lib import Person
-#
-# p = Person()
-# p.set_age(78)
-# print(p.get_name())
-# print(p.get_age())
-# p.person_info()
-
-
-# p._age = 856 # Доступ (редактирование) к атрибутам из вне класса (запрещается)
-# print(p._name)
-# print(p._age)
-
-
-# from lib import Car
-#
-# car = Car('BMW', 'M3', 'black')
-# #car.start_engine()
-# car.engine_on = True
-# car.drive_to('город')
-#
-# car2 = Car()
-# car2.start_engine()
-# car2.drive_to('на дачу')
-
-# Методы классов
-# class Greater:
-#     def hello(self,name = 'Noname') -> None:
-#         print('Привет,',name)
-#
-#     def goodbye(self) -> None:
-#         print('Пока, мир!')
-#
-# # self - ссылка на объект вызвавший класс
-#
-# g = Greater()
-# g.hello('Жека.')
-# g.goodbye()
-
-# Свойства классов
-# class Fruit:
-#     pass
-#
-# a = Fruit()
-# b = Fruit()
-# c = Fruit()
-#
-# a.name = 'Яблоко'
-# a.weight = 120
-#
-# b.name = 'Груша'
-# b.weight = 150
+#     @abstractmethod
+#     def perimetr(self):
+#         pass
 #
 #
-# print(a.name)
-# print(a.weight)
-# print(c.weight)
-
-# Регулярные выражения (поиск по паттерну)
-# Regular expressions (re)
-# r-строка - raw-string ("сырая" строка) - отключение всех управляющих символов
-# Квантификаторы (quantity)
-# {m} - ровно m раз
-# {m,}- m раз и более
-# {n,} - не более n раз
-# {m,n} - от m до n (без пробела)
-# ? - от нуля до одного (аналог {0,1})
-# * - от нуля до бесконечности (32767) {0,}
-# + - от 1 до бесконечности (32767) {1,}
-# import re
-# import requests
+# # Фигуры
+# class Circle(Shape):
+#     def __init__(self, radius):
+#         self.radius = radius
+#         self.name = 'круг'
 #
-# pattern = r'<img[^>]+src="([^">]+)"'
-# # test_string = 'img height="50" width="150" src="images/bg.jpg"'
-# html = requests.get('https://yandex.ru').text
-# result = re.findall(pattern, html)
-# print(result)
-
-# pattern = r'\b\w{4}\b' # все слова из 4 символов
-# pattern = r'\d'
-# pattern = r'd{3}' # три цифры подряд
-# pattern = r'начало!\Z' # строка заканчивается на "начало!"
-# pattern = '[0-5][0-9]' # последовательность из двух цифр
-# pattern = '[а-яА-я]'  # все буквы от а до я и от А до Я
-# pattern = '[^ерм]' # Исключить символы: [ерм]
-# pattern = r'\((.+?)\)' # вытащить текст из скобок
-# pattern = 'o{2,5}' # Вывести "о" которое встречается от 2 до 5 раз
-# pattern = 'Go{2,}gle'
-
-# pattern = r'стеклянн?ый' # 2-я "н" может присутствовать
-# "жадный" квантификатор (greedy quantifier)
-
-# pattern = r'<img.*>' # жадный(greedy) квантификатор
-# pattern = r'<img.*?>' # ленивый(lazy, non-greedy) квантификатор
-# pattern = r'<img[^>]+src="([^">]+)"' # только путь к картинке
-# pattern = r'<p>(.*?)</p>' # содержимое абзаца html
-# pattern = r'p[^>]*>(.*)</p>' # содержимое абзаца html c атрибутами
-# result = re.findall(pattern, test_string)
-
-# print('Цифры есть.') if result else print('Цифр нет.') # тернарный if (тернарный условный оператор) (три конца и никаких elif)
-
-# Убираем все знаки препинания
-# def remove_punctuation(input_str: str) -> str:
-#     """
-#     Методом sub() заменяем все найденные совпадения
-#     пустой строкой и возвращаем "очищенную"
-#     :param input_str: строка со знаками препинания
-#     :return: строку, очищенную от знаков препинания
-#     """
-#     return re.sub(r'[^\w\s]','',input_str)
-# test_string = ('Для 1 000 знаков текста подходят стр??????????аницы приветственных текстов,'
-#                ' описания больших карточек товаров, рекламные посты, короткие письма для рассылки,'
-#                ' малые тексты “О компании” и другие подобные форматы.')
+#     def perimetr(self):
+#         return round(2 * pi * self.radius, 2)
 #
-# pattern = r'[,.;:!]'
-# test_string = 'яблоко,           груша.      банан;          слива!абрикос'
-# # test_string = ''.join(test_string.split()) # убрали все пробелы
-# result = re.split(pattern, test_string) # убираем знаки по паттерну
+#     def area(self):
+#         return round(pi * self.radius ** 2, 2)
 #
-# # через map
-# # result = list(map(lambda x: x.strip(), result))
+#     def get_name(self):
+#         return self.name
 #
-# # через list comprehension с сортировкой
-# result = sorted(x.strip() for x in result)
-# print(result)
+#
+# class Rectangle(Shape):
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#         self.name = 'прямоугольник'
+#
+#     def perimetr(self):
+#         return 2 * (self.width + self.height)
+#
+#     def area(self):
+#         return self.width * self.height
+#
+#     def get_name(self):
+#         return self.name
+#
+#
+# class Square(Rectangle):
+#     def __init__(self, side):
+#         super().__init__(side, side)
+#         self.name = 'квадрат'
+#
+#
+# class Triangle(Square):
+#     def __init__(self, side):
+#         super().__init__(side)
+#         self.side = side
+#         self.name = 'треугольник'
+#
+#     def area(self):
+#         return (self.side ** 2 * 3 ** 0.5) / 4
+#
+#     def perimetr(self):
+#         return self.side * 3
+#
+#
+# s = Square(5)
+# print(s.area())
+# print(s.perimetr())
+# print(s.get_name())
+# s.info()
+#
+# circle = Circle(5)
+# print(circle.area())
+# print(circle.perimetr())
+# print(circle.get_name())
+# circle.info()
+#
+# tr = Triangle(8)
+# print(tr.area())
+# print(tr.perimetr())
+# print(tr.get_name())
+# tr.info()
 
-
-# Линтеры - статический анализатор кода (контролирует следование хорошим практикам)
-# Flake8 - самый популярный линтер
-# pip install flake8
-# (flake8-bugbear - для нахождения распространенных логических ошибок в коде)
-# (pep8-naming - проверяет имена на соответствие pep8)
-# pip install flake8-bugbear pep8-naming
-# Arguments: --max-complexity 10 $FileDir$/$FileName$
-# Path: $FileDir$
-# Advanced Options/OutputFilter: $FILE_PATH$:$LINES$
-
-# Библиотека pymorphy
-# pip install pymorphy3
-# pip install -U pymorphy3-dicts-ru
-# import pymorphy3
+# method override; operator overloading
+# __call__ - экземпляр класса становится вызываемым
+# (как функция)
+# y = ax^2 + bx + c
+# class SquareFunction:
+#     def __init__(self, a, b, c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
 #
-# form = pymorphy3.MorphAnalyzer().parse('бутылка')[0]
+#     def __call__(self, x):
+#         return self.a * x ** 2 + self.b * x + self.c
 #
-# for btl in reversed(range(99)):
-#     print(f'В холодильнике {btl + 1} {form.make_agree_with_number(btl+1).word} пива')
-#     print('Возьмем одну и выпьем.')
-#     if btl % 10 == 1 and btl != 11:
-#         remain = 'Осталась'
+#
+# s = SquareFunction(1, 2, 3)
+# print(s(2))
+
+# class MyTime:
+#     def __init__(self, minutes, seconds):
+#         if 0 <= minutes < 60:
+#             self.minutes = minutes
+#         if 0 <= seconds < 60:
+#             self.seconds = seconds
+#
+#     def __add__(self, other):
+#         m = self.minutes + other.minutes
+#         s = self.seconds + other.seconds
+#         m += s // 60
+#         s = s % 60
+#         m = m % 60
+#         return MyTime(m, s)
+#
+#     def __str__(self):
+#         return f'<Time {self.minutes:02}:{self.seconds:02}>'
+#
+#
+# t1 = MyTime(13, 0)
+# t2 = MyTime(53, 5)
+# print(t1 + t2)
+
+# from math import hypot
+#
+#
+# class Point:
+#     def __init__(self, x=0, y=0):
+#         self.x = x
+#         self.y = y
+#
+#     def __str__(self):
+#         return f'<Point: ({self.x}, {self.y})>'
+#
+#     def __repr__(self):
+#         return f'<List of Points: ({self.x}, {self.y})>'
+#
+#     def __sub__(self, other):
+#         return Point(abs(self.x - other.x), abs(self.y - other.y))
+#
+#     def __add__(self, other):
+#         return hypot(self.x - other.x, self.y - other.y)
+#
+#
+# p1 = Point(5, 4)
+# p2 = Point(10, 2)
+# print(p1 - p2)
+# print(p1 + p2)
+# str(a) -> a.__str__()
+# isinstance(объект, тип) -> True
+# isinstance(объект, (тип1,  тип 2, тип N)) -> True
+
+# lst = list(range(1, 15))
+# lst += ['a']
+#
+# class Stat:
+#     def __init__(self, vals):
+#         self.values = vals[:]  # получаем копию
+#
+#     def is_all_int(self) -> bool:
+#         return all(isinstance(item, int) for item in self.values)
+#
+#     def get_min(self):
+#         if self.is_all_int():
+#             return min(self.values)
+#         return None
+#
+#     def get_max(self):
+#         if self.is_all_int():
+#             return max(self.values)
+#         return None
+#
+#     def get_aver(self):
+#         if self.is_all_int():
+#             return sum(self.values) / len(self.values)
+#         return None
+#
+#
+# s = Stat(lst)
+# print(s.get_min())
+# print(s.get_max())
+# print(s.get_aver())
+
+# class Selector:
+#     def __init__(self, vals):
+#         self.values = vals[:]  # получаем копию
+#
+#     def get_odd(self):
+#         return [x for x in self.values if x % 2 == 1]
+#
+#     def get_even(self):
+#         return [x for x in self.values if x % 2 == 0]
+#
+#
+# s = Selector(lst)
+# print(s.get_odd())
+# print(s.get_even())
+# print(lst)
+
+# from lib import Student, Employee, Person
+#
+# people = [
+#     Person('Александр', 27),
+#     Student('Дмитрий', 'ГУАП'),
+#     Employee('Пётр', 'Авангард'),
+# ]
+#
+# for person in people:
+#     if isinstance(person, Student):
+#         print(person.get_univercity())
+#     elif isinstance(person, Employee):
+#         print(person.get_company())
 #     else:
-#         remain = 'Осталось'
-#     print(f'{remain} {btl} {form.make_agree_with_number(btl).word} пива.')
+#         print(person.get_name())
 
-# Исключения (runtime)
-# try:
-#   что пытаемся сделать
-# except:
-#   обрабатываем исключения
-# else:
-#   если исключения не было
-# finally:
-#   выполняется в любом случае
-
-
-# # Практикум (обучаемый словарь)
-# import pickle
+# from lib import Circle, Rectangle, Square
 #
-# # Минимальная версия, если файл dict.dat отсутствует
-# voc = {
-#     'стол': 'table',
-#     'стул': 'chair',
-# }
+# # def shape_info(shape: object):
+# #     print(f'Площадь {shape.get_name()}а: {shape.area()}, Периметр: {shape.perimetr()}')
+# rect, c, sqr = 'прямоугольник', 'круг', 'квадрат'
 #
 #
-# # функция для распечатки словаря
-# def print_voc():
-#     print('Сейчас словарь содержит: ')
-#     for k, v in voc.items():
-#         print(k, '—', v)
+# def shape_info(shape: object):
+#     if isinstance(shape, Circle):
+#         fig = c
+#     elif isinstance(shape, Rectangle):
+#         fig = rect
+#     elif isinstance(shape, Square):
+#         fig = sqr
+#     print(f'Площадь {fig}а: {shape.area()}, Периметр: {shape.perimetr()}')
 #
 #
-# # загружаем словарь из файлов
-# try:
-#     with open('dict.dat', 'rb') as dump_in:
-#         voc = pickle.load(dump_in)
+# s = Square(10)
+# shape_info(s)
 #
-# except FileNotFoundError:
-#     with open('dict.dat', 'wb') as dump_out:
-#         pickle.dump(voc,dump_out)
-#     print('Создан минимальный словарь')
-#     print_voc()
-# while True:
-#     temp = input('\nВведите слова для перевода или # для завершения: ')
-#     word = temp.strip().lower()
-#     if word == '#' or word == '№':
-#         break
-#     if word in voc.keys():
-#         translate = voc[word]
-#         print(f'Cлово "{word}" переводится как {translate}.\n')
-#     else:
-#         print(f'Значение слова {word} отсутствует в словаре')
-#         new_key = f'А как слово {word} переводится.\n '
-#         new_key += 'Если ничего не вводите нажмите ENTER,\n '
-#         new_key += 'Или введите его здесь: '
-#         new_word = input(new_key)
+# cr = Circle(10)
+# shape_info(cr)
 #
-#         if new_word != '' or len(new_word) > 2 :
-#             voc[word] = new_word
-#             print(f'Слово "{word}" с переводом "{new_word}" внесено в словарь.')
-#         else:
-#             print('Ничего не введено или слишком короткое слово.')
-#             continue
-# print('До новых встреч!')
-# # Сохранить словарь
-# with open('dict.dat', 'wb') as dump_out:
-#     pickle.dump(voc,dump_out)
+# r = Rectangle(5, 2)
+# shape_info(r)
 
+# print(dir(s))
+# print(dir(cr))
 
-# Задача 2.
-# while True:
-#     a = input('Введите первое число: ')
-#     b = input('Введите второе число: ')
+# from lib import Book
 #
-#     if a.isdigit() and b.isdigit():
-#         if int(b) == 0:
-#             print('На ноль делить нельзя')
-#         else:
-#             print(int(a) / int(b))
-#             break
-#     else:
-#         print('Вводить надо только числа')
-
-
-# while True:
-#     try:
-#         a = input('Введите первое число: ')
-#         b = input('Введите второе число: ')
-#         c = int(a) / int(b)
-#     except ValueError:
-#         print('Только целые числа.')
-#     except ZeroDivisionError:
-#         print('На ноль делить нельзя.')
-#     else:
-#         print(f'{a}/{b} = {int(c)}')
-#         break
-
-
-# Задача 1.
-
-# Мое решение.
-# lst = [1,2,3,4,5,6,7,8,9]
-# loop = True
-# while loop:
-#     try:
-#         index = int(input('Введите индекс: '))
-#         print(f'Число по индексу {index}: {lst[index]}')
-#     except IndexError:
-#         print('Вы вышли за диапазон.')
-#     except ValueError:
-#         print('Требуется ввести целое число.')
-#     else:
-#         loop = False
-
-# Решение преподавателя
-# lst = [1,2,3,4,5,6,7,8,9]
-# try:
-#     index = int(input('Введите индекс:'))
-#     if not -len(lst) < index < len(lst) - 1:
-#         raise ValueError('Индекс вне диапазона.')
-#     res = lst[index]
-#     print(f'Число по индексу {index}: {lst[index]}')
-# except ValueError as exp:
-#     mess = exp.args
-#     if mess[0].startswith('invalid literal'):
-#         print(f'Вводить надо числа')
-#     else:
-#         print(exp)
-
-
-# Утверждения (assertion)
-# В основном - для нужд тестирования
-# try:
-#     text = input('Введите текст: ')
-#     assert len(text) > 3 # это утверждение, если False -> получаем AssertionError
-# except AssertionError:
-#     print('Слишком коротки текст.')
-
-# "Бросаемся" исключениями - raise
-# max_val = 10
-# min_val = 1
+# book = Book('Язык С++', 'Бьярн Страупструп')
 #
-# try:
-#     val = int(input(f'Введите число в диапазоне от {min_val} до {max_val}: '))
-#     if not min_val < val < max_val:
-#         raise ValueError('Введенное число вне диапазона.')
-#     print(f'Введенное число {val} лежит в заданном диапазоне.')
+# print(f'{book.get_title(), book.get_author()}')
+
+# print(1 + 2)
+# print(1 + 2.0)
+# print('abc' + 'def')
+# print([1, 2] + [3, 4])
 #
-# except ValueError as exp:
-#     print('Надо быть внимательнее:',exp)
-
-# print('Остаток от деления:')
-# loop = True
-# while loop:
+# def func(x, y):
+#     return x + y
 #
-#     try:
-#         value = int(input('На что делим число 10:'))
-#         res = 10 % value
-#         print(f'Остаток от деления 10 на {value} = {res}')
-#     except ZeroDivisionError:
-#         print('На ноль делить нельзя!')
-#     except ValueError:
-#         print('Надо вводить только целые числа.')
-#     except Exception as exp: # Любое исключение в переменной exp
-#         print('Произошло исключение:', exp.__class__.__name__,'|',exp)
-#     else:
-#         loop = False
-
-###########################################
-# flag = False # открывался ли на запись
-#
-# try:
-#     fo = open('information.txt', encoding='utf-8')
-# except FileNotFoundError:
-#     fo = open('information.txt', 'wt', encoding='utf-8')
-#     flag = True
-#     print('Файл не обнаружен и создан с параметрами по умолчанию.')
-#     # with open('information.txt', 'wt', encoding='utf-8') as fo:
-#     #     fo.write('По умолчанию.')
-# else:
-#     print('Файл открыт успешно. Читаем его.')
-#     print(fo.read())
-#     fo.close()
-# finally:
-#     if flag:   # если файл был открыт на запись (было исключение)
-#         fo.write('По умолчанию.')
-#         fo.close()
-#     print('Продолжаем работать.')
-############################################
-
-# res = []
-# with open('info.txt', 'rt') as f:
-#    while temp := f.readline().rstrip('\n'):
-#        res += temp.split(', ')
-#
-# # res = set(list(map(lambda x: x.rstrip('\n'),res)))
-# # res = set(res)
-# res = sorted(int(x) for x in set(res))
-#
-# print(res)
-# from path_lib import *
-# print(img_dir)
-
-# import pickle
-# import pprint
-
-# d = {
-#     'стол': 'table',
-#     'стул': 'chair',
-# }
-
-# # Сериализация
-# with open('dictfile.dat', 'wb') as p:
-#     # d - что сериализуем
-#     # p - куда сериализуем
-#     pickle.dump(d, p)
-
-# # Десериализация
-# with open('dictfile.dat', 'rb') as p:
-#     d = pickle.load(p)
-# pprint.pprint(d,width=15)
+# print(func(2, 3.0))
