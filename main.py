8befe7e79c0f66e34d87bd24d0fcd0eb54d62305
BegBro <andrei.begunov322@gmail.com>
2025-07-15 15:53:33 +0300

    Decorator NonLocal

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 # Декораторы
-def upper_case_print(old_func):
-    def new_func(*args,**kwargs):
-        args_up_case = [str(arg).upper() for arg in args]
-        old_func(*args_up_case,**kwargs)
-    return new_func
+def outer():
+    x = 5
 
+    def inner():
+        nonlocal x
+        print('Nonlocal x=',x)
+        x = 10
+    inner()
+    print('New x=',x)
 
-new_print = upper_case_print(print)
-new_print('Привет, Пока')
+outer()
+
+# def upper_case_print(old_func):
+#     def new_func(*args, **kwargs):
+#         case = kwargs.pop('case', None)
+#         if case == 'U':
+#             args = [str(arg).upper() for arg in args]
+#         elif case == 'L':
+#             args = [str(arg).lower() for arg in args]
+#         return old_func(*args, **kwargs)
+#
+#     return new_func
+#
+#
+# new_print = upper_case_print(print)
+# new_print('Привет, Пока')
+# new_print('Привет,Пока', case='U')
+# new_print('Привет,Пока', case='L')
 
 # def answer(question):
 #     return 'думайте сами'
@@ -27,7 +46,6 @@ new_print('Привет, Пока')
 #
 #
 # dialog()
-
 
 
 # Погода через API
