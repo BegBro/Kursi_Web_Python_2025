5c6c397b9c073d73544845d94c80a1f1b40ed39a
BegBro <andrei.begunov322@gmail.com>
2025-07-16 11:02:34 +0300

    Flask First Page

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-# Строки (immutable, iterable)
-# Начало и окончание строки
-# startswith и endswith
+# Введение во Flask
+# MVC - (Model View Controller)
+from flask import Flask
 
-s= 'смотреть'
+app = Flask(__name__)
 
-if s.startswith('смо'):
-    print('Да')
 
-if s.endswith('еть'):
-    print('Тоже да.')
+@app.route('/')
+@app.route('/index')
+def index():
+    print('Вызвана функция index')
+    return 'Привет, Flask'
+
+
+@app.route('/about')
+def about():
+    print('Вызвана функция about')
+    return 'О нас'
+
+
+@app.route('/countdown')
+def cd():
+    lst = [str(x) for x in reversed(range(10))]
+    lst.append('Полетели!!!')
+    return '<br>'.join(lst)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
---
+++ old/main.py
@@ -0,0 +1,11 @@
+# Строки (immutable, iterable)
+# Начало и окончание строки
+# startswith и endswith
+
+s= 'смотреть'
+
+if s.startswith('смо'):
+    print('Да')
+
+if s.endswith('еть'):
+    print('Тоже да.')
