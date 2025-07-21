039fc257396ba11143594d928b08e8adfc8c44db
BegBro <andrei.begunov322@gmail.com>
2025-07-21 12:15:57 +0300

    Lesson 18 and half Lesson 19

--- data/users.py
+++ data/users.py
@@ -1,9 +1,13 @@
 import datetime
 import sqlalchemy
-from .db_sessions import SqlAlchemyBase
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
 
 
-class User(SqlAlchemyBase):
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
@@ -11,7 +15,6 @@ class User(SqlAlchemyBase):
                            autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String,
                              nullable=True)
-
     about = sqlalchemy.Column(sqlalchemy.String,
                               nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
@@ -21,3 +24,17 @@ class User(SqlAlchemyBase):
                                         nullable=True)
     create_data = sqlalchemy.Column(sqlalchemy.DateTime,
                                     default=datetime.datetime.now())
+
+    news = orm.relationship("News", back_populates='user')
+
+    def set_username(self, newname):
+        self.name = newname
+
+    def __repr__(self):
+        return f'<User: {self.name}>'
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- forms/loginform.py
+++ forms/loginform.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField,
-                     BooleanField, SubmitField)
+from wtforms import StringField, PasswordField
+from wtforms import BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired('Это обязательное поле!')])
-    password = PasswordField('Пароль', validators=[DataRequired('Это обязательное поле!')])
+    email = StringField('Логин', validators=[DataRequired('Это обязательное поле!')])
+    password = PasswordField('Пароль', validators=[DataRequired('Без пароля нельзя!')])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
